ff15cb25080bb77dcc912bd7deaf1d50f3d07b64
CodeByBlazej <codebyblazej@gmail.com>
2025-11-04 18:06:31 +0000

    add validations and params to user.new in controller

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,10 @@
 class User < ApplicationRecord
   validates :name, presence: true, uniqueness: true
 
+  validates :email, uniqueness: true, presence: true
+
+  validates :password, presence: true
+
   has_many :posts
   # has_many :comments
 end
