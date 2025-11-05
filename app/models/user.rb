56cd969150605907397b4b5db9b7457d7e3db926
CodeByBlazej <codebyblazej@gmail.com>
2025-11-05 18:28:22 +0000

    add devise, and before_action to controller

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,14 @@
 class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+
   validates :name, presence: true, uniqueness: true
 
-  validates :email, uniqueness: true, presence: true
+  # validates :email, uniqueness: true, presence: true
 
-  validates :password, presence: true
+  # validates :password, presence: true
 
   has_many :posts
   # has_many :comments
